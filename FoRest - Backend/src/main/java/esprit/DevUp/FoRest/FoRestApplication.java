6e5bc2672ce8eb157b546a7bbb7d914b550bf02e
GitMeSoftly <tarek.hammami@esprit.tn>
2023-02-11 22:26:42 +0100

    First Commit
    Added a maven empty project

---
+++ FoRest - Backend/src/main/java/esprit/DevUp/FoRest/FoRestApplication.java
@@ -0,0 +1,13 @@
+package esprit.DevUp.FoRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoRestApplication.class, args);
+	}
+
+}

372af16b760fd30d723953ebacd9a5355b1d4921
Rayen <saadallahrayen@gmail.com>
2023-02-12 23:29:57 +0100

    init gestion Restaurant

---
+++ FoRest - Backend/src/main/java/esprit/DevUp/FoRest/Entity/User.java
@@ -0,0 +1,20 @@
+package esprit.DevUp.FoRest.Entity;
+
+import java.io.Serializable;
+import lombok.*;
+import javax.persistence.*;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Entity
+
+public class User  {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_user", nullable = false)
+    private Integer idUser;
+
+}
--- FoRest - Backend/src/main/java/esprit/DevUp/FoRest/FoRestApplication.java
+++ FoRest - Backend/src/main/java/esprit/DevUp/FoRest/FoRestApplication.java
@@ -2,8 +2,10 @@ package esprit.DevUp.FoRest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class FoRestApplication {
 
 	public static void main(String[] args) {
